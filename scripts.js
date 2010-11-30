ee3e73681c6bef0c53598839d0afd812f2749a1e
Justin Forest <justin.forest@gmail.com>
2010-11-30 12:39:23 +0300

    Web site udpdate.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 	$('a.external').attr('target', '_blank');
 	$('a[href*="://"]:not([href^="http://beta.deadchannel.ru/"])').attr('target', '_blank');
+
+	$('#submitlink').click(function(){
+		$('#submit').show('slow');
+	});
+
+	$('#submitlink textarea').focus(function(){
+		$(this).html('lala.');
+	});
 });
